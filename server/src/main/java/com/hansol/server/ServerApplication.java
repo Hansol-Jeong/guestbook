702ad0cabd0a150a2be993fc1b0246b63cbfd76c
Hansol-Jeong <shn03014@naver,com>
2021-03-07 05:30:17 +0900

    Feat: Divided yml for security

--- server/src/main/java/com/hansol/server/ServerApplication.java
+++ server/src/main/java/com/hansol/server/ServerApplication.java
@@ -2,13 +2,19 @@ package com.hansol.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 @SpringBootApplication
 @EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 public class ServerApplication {
+    public static final String PropertyPath = "spring.config.location="
+            +"classpath:application.yml,"
+            +"optional:classpath:database.yml";
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
+        new SpringApplicationBuilder(ServerApplication.class)
+                .properties(PropertyPath)
+                .run(args);
     }
 }
