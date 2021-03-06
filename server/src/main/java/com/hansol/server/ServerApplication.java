226a6925525844cb94e1630b4887c1fcdc3d595e
Hansol-Jeong <shn03014@naver,com>
2021-03-07 03:07:58 +0900

    Feat: git ignore database.yml

--- server/src/main/java/com/hansol/server/ServerApplication.java
+++ server/src/main/java/com/hansol/server/ServerApplication.java
@@ -2,12 +2,13 @@ package com.hansol.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 @SpringBootApplication
 @EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
+@PropertySource(value = "classpath:database.yml")
 public class ServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
